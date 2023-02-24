cad6bf376e72a344b4677c344f2e9638b2c9ebf9
1kak19tus9 <115793707+1kak19tus9@users.noreply.github.com>
2023-02-24 20:30:45 +0700

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,18 @@
+import mylib
+
+mylib.vyvodPolya(mylib.vidimost_polya)
+game = True
+while game:
+    stroka = int(input("Введите номер строки"))
+    stolb = int(input("Введите номер столбца"))
+    while stroka <= 1 or stroka >= 12 or stolb <= 1 or  stolb >= 12:
+            stroka = int(input("Введите номер строки"))
+            stolb = int(input("Введите номер столбца"))
+    mylib.check(stroka-1,stolb-1)
+    mylib.vyvodPolya(mylib.vidimost_polya)
+    if mylib.isOpen():
+        game = False
+        
+print("Всё поле открыто!")
+
+
