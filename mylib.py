cb0bcd4a586dcba0e28381eeb4de5babb944faff
1kak19tus9 <115793707+1kak19tus9@users.noreply.github.com>
2023-03-03 20:29:14 +0700

    Add files via upload

--- mylib.py
+++ mylib.py
@@ -25,57 +25,57 @@ vidimost_polya=[["•","•","•","•","•","•","•","•","•","•","�
                 ["•","•","•","•","•","•","•","•","•","•","•","•"],
                 ["•","•","•","•","•","•","•","•","•","•","•","•"],
                 ["•","•","•","•","•","•","•","•","•","•","•","•"]]
-for i in range(5):
-    e = random.randint(1,12)
-    t = random.randint(1,12)
+bombik = 5
+for i in range(bombik):
+    e = random.randint(1,11)
+    t = random.randint(1,11)
     vidimost_polya[e][t] = "&"
 def vyvodPolya(spisok):
     for stroka in spisok:
         for kletka in stroka:
             print(kletka,end='')
         print()
-        
+
 def check(stroka,stolb):
     if vidimost_polya[stroka][stolb] == "•":
-       
+
         vidimost_polya[stroka][stolb] = pole[stroka][stolb]
-        
+
         if pole[stroka][stolb] == "*":
-            
+
             if stroka-1 >= 0:
                 check(stroka-1,stolb)
                 if stolb-1 >= 0:
                     check(stroka-1,stolb-1)
                 if stolb+1 < len(pole[stroka]):
                     check(stroka-1,stolb+1)
-                    
+
             if stroka+1 < len(pole):
                 check(stroka+1,stolb)
                 if stolb-1 >= 0:
                     check(stroka+1,stolb-1)
                 if stolb+1 < len(pole[stroka]):
                     check(stroka+1,stolb+1)
-                    
+
             if stolb-1 >= 0:
                     check(stroka,stolb-1)
             if stolb+1 < len(pole[stroka]):
                 check(stroka,stolb+1)
 
 def isOpen():
-    
+
     opened = True
-   
+
     for stroka in vidimost_polya:
-      
+
         if "•" in stroka:
-          
+
             opened = False
+        else:
+            opened = True
+            print("Игра окончена")
     return opened
-
-
-
-
-
-
-
+def bomb():
+    if stroka +1 == 
+        
 
