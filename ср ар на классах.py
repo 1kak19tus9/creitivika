abc03d38408fcba5b1c2022db3ff9b530e7a9f87
1kak19tus9 <115793707+1kak19tus9@users.noreply.github.com>
2023-09-22 20:12:46 +0700

    Add files via upload

---
+++ ср ар на классах.py
@@ -0,0 +1,11 @@
+class ch():
+    def __init__(self, one, two):
+        self.one = one
+        self.two = two
+    def su(self):
+        d = ((self.one+self.two)/2)
+        print(d)
+on = int(input())
+tw = int(input())
+a = ch(on,tw)
+a.su()
